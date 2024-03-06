cf334a01185e36f3ffadc9a04ff4f1678326bcac
James Alan Bush <jamesalanbush1972@gmail.com>
2024-03-06 01:45:24 -0500

    Initial Commit

---
+++ DemonCam-Swift-Metal/DemonCam_Swift_MetalApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemonCam_Swift_MetalApp.swift
+//  DemonCam-Swift-Metal
+//
+//  Created by Xcode Developer on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct DemonCam_Swift_MetalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
